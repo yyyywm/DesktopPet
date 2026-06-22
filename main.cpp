7c015bd08675329ffbeb40c4b429e8f37611411b
yyyywm <a1969334998@hotmail.com>
2026-06-23 02:10:33 +0800

    feat(app): switch main entry to QGuiApplication and QML engine

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
+#include <memory>
 
-#include "main_window.h"
-
-#include <QApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include <QDir>
 
 #include "core/config_store.h"
 #include "core/event_repository.h"
 #include "core/qsettings_adapter.h"
 #include "services/update_service.h"
+#include "ui/application_controller.h"
+#include "ui/task_list_model.h"
 
-int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
+int main(int argc, char* argv[]) {
+  QGuiApplication app(argc, argv);
 
   const QString config_path =
       QDir::current().filePath(QStringLiteral("../config/eventlist.ini"));
@@ -20,10 +23,28 @@ int main(int argc, char *argv[]) {
       std::make_unique<desktop_todo::core::ConfigStore>(std::move(settings));
   desktop_todo::core::EventRepository event_repository(config_store.get());
   event_repository.Load();
+  qDebug() << "Loaded" << event_repository.events().size() << "events";
 
   desktop_todo::services::UpdateService update_service;
+  desktop_todo::ui::TaskListModel task_model(&event_repository);
+  desktop_todo::ui::ApplicationController controller(&update_service);
 
-  MainWindow w(nullptr, &event_repository, &update_service);
-  w.show();
-  return a.exec();
+  QQmlApplicationEngine engine;
+  engine.rootContext()->setContextProperty(
+      QStringLiteral("TaskModel"), &task_model);
+  engine.rootContext()->setContextProperty(QStringLiteral("App"), &controller);
+
+  const QUrl url(QStringLiteral("qrc:/DesktopPet/qml/main.qml"));
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject* obj, const QUrl& obj_url) {
+        if (obj == nullptr && url == obj_url) {
+          qCritical() << "Failed to load main QML:" << url;
+          QCoreApplication::exit(-1);
+        }
+      },
+      Qt::QueuedConnection);
+  engine.load(url);
+
+  return app.exec();
 }
